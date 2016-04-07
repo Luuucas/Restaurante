dae63d827f3104d3d27802e66b9d2bbc5a076006
Lucas Fernandes de Arruda <fernandes.85@hotmail.com>
2016-04-07 16:19:41 -0300

    commit construtor

---
+++ src/restaurante/Pedido.java
@@ -0,0 +1,31 @@
+package restaurante;
+
+import java.util.Date;
+
+
+public class Pedido {
+	
+	private long id;
+	private Date data; 
+	private String status; 
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
